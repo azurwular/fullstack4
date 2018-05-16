4cdc426f6c0e9a8c8379f3d66714b82d309b8439
dimitri <cph-dt46@cphbusiness.dk>
2018-05-16 11:08:49 +0200

    added

---
+++ facades/dbSettings.js
@@ -0,0 +1,12 @@
+const  DB_URI = "mongodb://azurwular:Akkraal25410*!@ds111299.mlab.com:11299/cos_library";
+const TEST_DB_URI = "mongodb://azurwular:Akkraal25410*!@ds111299.mlab.com:11299/cos_library";
+
+//default timeout for Mocha async tests
+const MOCHA_TEST_TIMEOUT = 5000;
+
+
+module.exports = {
+  DB_URI,
+  TEST_DB_URI,
+  MOCHA_TEST_TIMEOUT
+}
